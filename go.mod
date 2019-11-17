aaafbd41c0400d80ab67e3dcbc3bdedda6c7512c
unknown <jamespaulmoriarty@gmail.com>
2019-11-17 12:57:03 +1100

    refinement: code formating.

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
-	golang.org/x/tools v0.0.0-20191115202509-3a792d9c32b2 // indirect
+	golang.org/x/tools v0.0.0-20191116214431-80313e1ba718 // indirect
 )
--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
 	var (
-		processName = "csgo.exe"
-		moduleName  = "client_panorama.dll"
-		// https://docs.microsoft.com/en-gb/windows/win32/inputdev/virtual-key-codes
-		VKSPACE = 0x20
-		// https://github.com/frk1/hazedumper/blob/master/csgo.cs
+		// Constants
+		PROCESSNAME = "csgo.exe"
+		MODULENAME  = "client_panorama.dll"
+		VKSPACE     = 0x20 // https://docs.microsoft.com/en-gb/windows/win32/inputdev/virtual-key-codes
+		// Player flags https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/public/const.h#L147
+		playerFlagsJump = uintptr(0x6)
+		// Offsets https://github.com/frk1/hazedumper/blob/master/csgo.cs
 		offsetLocalPlayerFlags = uintptr(0x104)
 		offsetLocalPlayer      = uintptr(0xCFAA3C)
 		offsetForceJump        = uintptr(0x51B0758)
-		// Flags
-		flagsJump = uintptr(0x6)
 		// Dynamic adresses
 		addressBase             uintptr
 		addressForceJump        uintptr
@@ -26,9 +26,9 @@ func main() {
 		addressLocalPlayerFlags uintptr
 	)
 
-	pid, success := win32.GetProcessID(processName)
+	pid, success := win32.GetProcessID(PROCESSNAME)
 	fmt.Println("Info: GetProcessID", success, pid)
-	module, success, address := win32.GetModule(moduleName, pid)
+	module, success, address := win32.GetModule(MODULENAME, pid)
 	fmt.Println("Info: GetModule", module.ModBaseSize, success, address)
 	process, err := win32.OpenProcess(win32.PROCESS_ALL_ACCESS, false, pid)
 	fmt.Println("Info: OpenProcess", process, err)
@@ -48,7 +48,7 @@ func main() {
 			win32.ReadProcessMemory(process, win32.LPCVOID(addressLocalPlayerFlags), &flagsCurrent, 1)
 
 			if flagsCurrent != 0 {
-				win32.WriteProcessMemory(process, addressForceJump, unsafe.Pointer(&flagsJump), 1)
+				win32.WriteProcessMemory(process, addressForceJump, unsafe.Pointer(&playerFlagsJump), 1)
 			}
 		}
 		time.Sleep(5)
