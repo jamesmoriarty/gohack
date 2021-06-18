86adba768da3d4304c4d9c03de2aa3d325b87dc6
James Moriarty <jamespaulmoriarty@gmail.com>
2021-06-18 18:37:56 +1000

    refinement: changes for presentation.

--- internal/gohack/trigger.go
+++ internal/gohack/trigger.go
@@ -6,15 +6,19 @@ import (
 	"github.com/jamesmoriarty/gomem"
 )
 
+const (
+	VK_SHIFT         = 0x10 // https://docs.microsoft.com/en-gb/windows/win32/inputdev/virtual-key-codes
+	CSGO_FORCEATTACK = 0x6
+)
+
 func RunTrigger(client *Client) {
 	for {
-		if gomem.IsKeyDown(0x10) { // https://docs.microsoft.com/en-gb/windows/win32/inputdev/virtual-key-codes
+		if gomem.IsKeyDown(VK_SHIFT) {
 			if client.OffsetEntityId() > 0 && client.OffsetEntityId() <= 64 {
-				client.Process.WriteByte(client.OffsetForceAttack(), 0x6)
+				client.Process.WriteByte(client.OffsetForceAttack(), CSGO_FORCEATTACK)
 			}
 		}
 
-		time.Sleep(50)
+		time.Sleep(50 * time.Millisecond)
 	}
-
 }
