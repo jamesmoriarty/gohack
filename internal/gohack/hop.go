01ee7998d869a8e07495c5d8c390953bf434c21e
unknown <jamespaulmoriarty@gmail.com>
2021-06-20 13:57:50 +1000

    refinement: remove comment.

--- internal/gohack/hop.go
+++ internal/gohack/hop.go
@@ -17,7 +17,7 @@ func RunHop(client *Client) {
 		if gomem.IsKeyDown(VK_SPACE) {
 			flags, _ := client.Process.ReadByte(client.OffsetPlayerFlags())
 
-			if (flags & CSGO_FL_ONGROUND) > 0 { // FL_ONGROUND (1<<0) // https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/public/const.h
+			if (flags & CSGO_FL_ONGROUND) > 0 {
 				client.Process.WriteByte(client.OffsetForceJump(), CSGO_FORCEJUMP)
 			}
 		}
