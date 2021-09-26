37b4818ccd638d9370618819a2fca400580cb0e0
James Moriarty <jamespaulmoriarty@gmail.com>
2021-09-26 10:33:47 +1000

    bugfix: line ending seq.

--- bin/build.bat
+++ bin/build.bat
@@ -1,24 +1,24 @@
 go version
 
-REM configure csgo.exe dist is 386^
+REM configure csgo.exe dist is 386
 set GOARCH=386
 set GOOS=windows
 set CGO_ENABLED=1
 set PATH=%PATH%;C:\MinGW\bin
 
-REM build .\test\dll\csgo.exe^
+REM build .\test\dll\csgo.exe
 cd .\test\dll
 gcc -c -o client.o client.c
 gcc -o client.dll -s -shared client.o -Wl,--subsystem,windows
 gcc -o csgo csgo.c -L. -lclient
 cd ..\..
 
-REM build .\test\nodll\csgo.exe^
+REM build .\test\nodll\csgo.exe
 cd .\test\nodll
 go build -v -o csgo.exe main.go
 cd ..\..
 
-REM build .\gohack.exe^
+REM build .\gohack.exe
 set GOARCH=amd64
 
 for /f %%a in ('powershell -Command "git rev-parse --short HEAD"') do set VERSION=%%a
@@ -28,5 +28,5 @@ go build -v -ldflags "-X github.com/jamesmoriarty/gohack.Version=%VERSION% -X gi
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM test^
+REM test
 go test -covermode=atomic -coverprofile='cover.out' -v ./...
